070f4cdcd466a53d764a419c4489a5997f36140f
Lawrence Willie <lawd96techworld@gmail.com>
2024-06-21 00:33:30 +0200

    project

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "60%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";  
+}
+
+
---
+++ js/update.js
@@ -0,0 +1,11 @@
+function update(){
+    document.getElementById("updates");
+
+    alert("This feature is not yet available. Call : 078 352 1065 or email :msizaeira@gmail.com");
+}
+
+function social_media(){
+    document.getElementById("whatsapp");
+    alert("Call : 078 352 1065 or email :msizaeira@gmail.com");
+}
+
